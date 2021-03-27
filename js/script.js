b8ab28b703b8a8fcc9bcc0a1403a23de22205885
Edivaldo Ferreira De Souza Júnior <ediarts7@gmail.com>
2021-03-27 01:00:41 -0300

    Adicionando sistema de game over

--- js/script.js
+++ js/script.js
@@ -45,6 +45,13 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game over :(");
+        }
+    } 
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -57,7 +64,12 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else {
+        food.x = Math.floor(Math.random()*15 + 1) * box;
+        food.y = Math.floor(Math.random()*15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
